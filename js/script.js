80f2e8138202f7fe4d750d82a87d830da4dda4c2
Unnati Pitla <unnatipitla1026@gmail.com>
2026-02-26 16:35:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log("Day 1 Task Loaded");
+
+/* Hamburger */
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelectorAll(".nav-menu a");
+const body = document.body;
+
+/* Toggle Menu */
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("nav-open");
+  body.classList.toggle("no-scroll");
+});
+
+/* Auto Close on Link Click */
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("nav-open");
+    body.classList.remove("no-scroll");
+  });
+});
