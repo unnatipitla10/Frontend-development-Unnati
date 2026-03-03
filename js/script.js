6b366ffd1f30c1e0d3b692ad1af6e498fbec8a35
Unnati Pitla <unnatipitla1026@gmail.com>
2026-03-03 16:47:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,5 +20,30 @@ function closePopup(){
     document.getElementById("popup").style.display="none";
 }
 
+//Notification Banner 
+function showNotification(type){
+    const container = document.getElementById("notificationContainer");
+
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+
+    notification.innerHTML = `
+        <span>${type === "success" ? "✅ Success: Project saved!" : "⚠ Error: Failed to fetch data."}</span>
+        <button class="close-btn">&times;</button>
+    `;
+
+    container.appendChild(notification);
+
+    // Auto remove after 5 seconds
+    setTimeout(()=>{
+        notification.remove();
+    },5000);
+
+    // Manual close
+    notification.querySelector(".close-btn").addEventListener("click",()=>{
+        notification.remove();
+    });
+}
+
 
 
